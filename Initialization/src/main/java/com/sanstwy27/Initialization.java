6ae3e1939a2642c5b62943ccad03f382aea8386e
sanstwy777 <sanstwy777@outlook.com>
2020-08-17 15:29:23 +0800

    Initialization

---
+++ Initialization/src/main/java/com/sanstwy27/Initialization.java
@@ -0,0 +1,14 @@
+package com.sanstwy27;
+
+/**
+ * @author Sanstwy27
+ * @create 8/17/2020
+ */
+
+public class Initialization {
+    public static void main(String[] args) {
+        Son s1 = new Son(); // 5 1 10 6 9 3 2 9 8 7
+        System.out.println();
+        Son s2 = new Son(); // 9 3 2 9 8 7
+    }
+}
