76e545eca5152257b896b4b5fe7d6a4beffa647e
sanstwy777 <sanstwy777@outlook.com>
2020-08-17 10:00:29 +0800

    rename class to HelloWorld

--- helloworld/src/main/java/com/sanstwy27/HelloWorld.java
+++ helloworld/src/main/java/com/sanstwy27/HelloWorld.java
@@ -5,7 +5,7 @@ package com.sanstwy27;
  * @create 8/17/2020
  */
 
-public class javanotes {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
