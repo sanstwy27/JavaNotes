550585176664bb8aeab3f7d998c8b378d59f032d
sanstwy777 <sanstwy777@outlook.com>
2020-08-17 09:56:30 +0800

    initial

---
+++ helloworld/src/main/java/com/sanstwy27/javanotes.java
@@ -0,0 +1,12 @@
+package com.sanstwy27;
+
+/**
+ * @author Sanstwy27
+ * @create 8/17/2020
+ */
+
+public class javanotes {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
