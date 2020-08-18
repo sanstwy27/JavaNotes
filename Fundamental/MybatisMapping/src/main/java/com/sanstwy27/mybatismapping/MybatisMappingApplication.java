bba96f9dc15c8e36e3cbae5c7e08e84417438bb6
sanstwy777 <sanstwy777@outlook.com>
2020-08-19 06:57:11 +0800

    MybatisMapping

---
+++ Fundamental/MybatisMapping/src/main/java/com/sanstwy27/mybatismapping/MybatisMappingApplication.java
@@ -0,0 +1,13 @@
+package com.sanstwy27.mybatismapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisMappingApplication.class, args);
+    }
+
+}
---
+++ Fundamental/MybatisMapping/src/test/java/com/sanstwy27/mybatismapping/MybatisMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanstwy27.mybatismapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
