dabe87f7b6a2f6be54558b16370c2fda5847b8ef
sanstwy777 <sanstwy777@outlook.com>
2020-08-20 07:29:56 +0800

    1. move Singleton module to Concurrent folder 2. update Singleton5[Double Check Lock] problem

--- Concurrent/Singleton/src/main/java/com/sanstwy27/Singleton5.java
+++ Concurrent/Singleton/src/main/java/com/sanstwy27/Singleton5.java
@@ -7,10 +7,22 @@ package com.sanstwy27;
  */
 
 public class Singleton5 {
-    private static Singleton5 INSTANCE;
+    // Why volatile?
+    // consider [INSTANCE = new Singleton5();]
+    //
+    // Pseudocode
+    // 1. memory = allocate();
+    //    instance(memory);  ----> instance initialized
+    //    instance = memory; ----> instance != null
+    //
+    // 2. memory = allocate();
+    //    instance = memory; ----> instance != null
+    //    instance(memory);  ----> instance initialized
+    private volatile static Singleton5 INSTANCE;
     private Singleton5() {
 
     }
+
     public static Singleton5 getInstance() throws InterruptedException {
         if (INSTANCE == null) {
             synchronized (Singleton5.class) {
