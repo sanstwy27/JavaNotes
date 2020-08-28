c1910fc95b04a0bdc952a90a010f4d2a4e7ba12d
sanstwy777 <sanstwy777@outlook.com>
2020-08-28 14:55:37 +0800

    rename CollectionProblemApplication to CollectionNotSafeApplication

--- Concurrent/CollectionProblem/src/main/java/com/sanstwy27/collectionproblem/CollectionNotSafeApplication.java
+++ Concurrent/CollectionProblem/src/main/java/com/sanstwy27/collectionproblem/CollectionNotSafeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
 
-public class CollectionProblemApplication {
+public class CollectionNotSafeApplication {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
