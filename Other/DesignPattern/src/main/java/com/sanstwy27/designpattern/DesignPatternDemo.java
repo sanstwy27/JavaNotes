d9fc9602a1d44a581cf0fdf40874403698148ef0
sanstwy777 <sanstwy777@outlook.com>
2020-08-30 03:47:15 +0800

    add DesignPattern module

---
+++ Other/DesignPattern/src/main/java/com/sanstwy27/designpattern/DesignPatternDemo.java
@@ -0,0 +1,13 @@
+package com.sanstwy27.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternDemo.class, args);
+    }
+
+}
